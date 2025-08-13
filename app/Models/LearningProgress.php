98f0c0087994e219dd38fb3b800c460aecca4e56
NajwanCF <najwan12311@gmail.com>
2025-08-13 13:54:12 +0700

    Perubahan nama file yang benar

--- app/Models/LearningProgress.php
+++ app/Models/LearningProgress.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class LearningProgress extends Model
 {
-    protected $table = 'learning_progress';
+    protected $table = 'learning_Progress';
 
     protected $fillable = [
         'id_user',
